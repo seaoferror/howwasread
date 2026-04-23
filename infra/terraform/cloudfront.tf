c9558baed02542ed8a21256c2a6b0b7baaf11f7f
jungwook <mr4454@naver.com>
2026-04-23 23:25:40 +0900

    fix: change legacy policy to managed cache and security policy for free plan
    Before if I custom my policy, aws cloudfront plan become pay-as-you-go,
    this is not what is wanted, so we use managed policy which behave same as
    before but in free plan.

--- infra/terraform/cloudfront.tf
+++ infra/terraform/cloudfront.tf
@@ -24,30 +24,6 @@ resource "aws_cloudfront_origin_access_control" "s3_oac" {
   signing_protocol                  = "sigv4"
 }
 
-resource "aws_cloudfront_response_headers_policy" "security_headers" {
-  name    = "standard-security-headers-policy"
-  comment = "Applies standard security headers to all responses"
-
-  security_headers_config {
-    frame_options {
-      frame_option = "DENY"
-      override     = true
-    }
-    content_type_options {
-      override = true
-    }
-    referrer_policy {
-      referrer_policy = "strict-origin-when-cross-origin"
-      override        = true
-    }
-    xss_protection {
-      protection = true
-      mode_block = true
-      override   = true
-    }
-  }
-}
-
 # Create the CloudFront Distribution
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   enabled = true
@@ -67,14 +43,9 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     # THIS enforces that users MUST use a Signed URL or Cookie
     trusted_key_groups = [aws_cloudfront_key_group.signer_key_group.id]
 
-    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
+    response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03" # Managed-SecurityHeadersPolicy
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # Managed-CachingOptimized
   }
 
   restrictions {
