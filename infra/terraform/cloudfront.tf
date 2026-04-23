64bde1e2e2267eeb3561cd79da12b1a4d0ea837f
jungwook <mr4454@naver.com>
2026-04-23 17:02:55 +0900

    feat: cloud front security header policy
    This will prevent malicious user manipulate with
    Content-type header.

--- infra/terraform/cloudfront.tf
+++ infra/terraform/cloudfront.tf
@@ -24,6 +24,30 @@ resource "aws_cloudfront_origin_access_control" "s3_oac" {
   signing_protocol                  = "sigv4"
 }
 
+resource "aws_cloudfront_response_headers_policy" "security_headers" {
+  name    = "standard-security-headers-policy"
+  comment = "Applies standard security headers to all responses"
+
+  security_headers_config {
+    frame_options {
+      frame_option = "DENY"
+      override     = true
+    }
+    content_type_options {
+      override = true
+    }
+    referrer_policy {
+      referrer_policy = "strict-origin-when-cross-origin"
+      override        = true
+    }
+    xss_protection {
+      protection = true
+      mode_block = true
+      override   = true
+    }
+  }
+}
+
 # Create the CloudFront Distribution
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   enabled = true
@@ -42,6 +66,8 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
 
     # THIS enforces that users MUST use a Signed URL or Cookie
     trusted_key_groups = [aws_cloudfront_key_group.signer_key_group.id]
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
 
     forwarded_values {
       query_string = false
