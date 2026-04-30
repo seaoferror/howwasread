1ed308bfea5c757e917ce55c53710aa65aa3d896
jungwook <mr4454@naver.com>
2026-04-30 18:32:27 +0900

    feat: VoiceInput component
    VoiceInput is child component which have MessageInput's setIsVoice(setState) function as props to destroy itself.
    When user press mic button in MessageInput component, VoiceInput component will render because isVoice state become true, and record start by useEffect.
    And when user discard the voice message recorded or recording by trash bin button or send the recorded voice message with another function prop handleFileMessage, after pause record or send message, setIsVoice(false) is called and clean up the VoiceInput component itself and show InputField component for text message.
    So lifecycle is fully managed by react and we dont createAudioPlayer or recorder.

---
+++ client/mobile/components/chat/VoiceInput.tsx
@@ -0,0 +1,126 @@
+import { Pressable, StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "@/constants";
+import InputField from "@/components/InputField";
+import {
+  RecordingPresets,
+  useAudioPlayer,
+  useAudioPlayerStatus,
+  useAudioRecorder,
+  useAudioRecorderState,
+} from "expo-audio";
+import { type Dispatch, type SetStateAction, useEffect } from "react";
+import { formatToMinuteSecond } from "@/util/time";
+
+interface VoiceInputProps {
+  setIsVoice: Dispatch<SetStateAction<boolean>>;
+  handleFileMessage: (
+    contentType: string,
+    mimeType: string,
+    content: string[],
+  ) => Promise<void>;
+}
+
+export default function VoiceInput({
+  setIsVoice,
+  handleFileMessage,
+}: VoiceInputProps) {
+  const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
+  const recorderState = useAudioRecorderState(audioRecorder);
+  const audioPlayer = useAudioPlayer();
+  const audioStatus = useAudioPlayerStatus(audioPlayer);
+
+  useEffect(() => {
+    const wrapper = async () => {
+      await audioRecorder.prepareToRecordAsync();
+      audioRecorder.record();
+    };
+
+    wrapper();
+  }, []);
+
+  return (
+    <InputField
+      value={
+        recorderState.isRecording
+          ? `${formatToMinuteSecond(recorderState.durationMillis / 1000)} recording...`
+          : audioPlayer.playing
+            ? `${formatToMinuteSecond(audioStatus.currentTime)} playing...`
+            : `${formatToMinuteSecond(audioStatus.duration)}`
+      }
+      editable={false}
+      leftChild={
+        <View style={styles.buttons}>
+          <Pressable
+            style={styles.buttonContainer}
+            onPress={async () => {
+              await audioRecorder.stop();
+              setIsVoice(false);
+            }}
+          >
+            <Ionicons name="trash-bin" size={20} color={colors.RED_500} />
+          </Pressable>
+          {!recorderState.isRecording &&
+            (audioStatus.playing ? (
+              <Pressable
+                style={styles.buttonContainer}
+                onPress={async () => {
+                  audioPlayer.pause();
+                  await audioPlayer.seekTo(0);
+                }}
+              >
+                <Ionicons name="stop" size={20} color="black" />
+              </Pressable>
+            ) : (
+              <Pressable
+                style={styles.buttonContainer}
+                onPress={async () => {
+                  audioPlayer.play();
+                }}
+              >
+                <Ionicons name="play" size={20} color="black" />
+              </Pressable>
+            ))}
+        </View>
+      }
+      rightChild={
+        recorderState.isRecording ? (
+          <Pressable
+            style={styles.buttonContainer}
+            onPress={async () => {
+              await audioRecorder.stop();
+              audioPlayer.replace(audioRecorder.uri);
+            }}
+          >
+            <Ionicons name="stop" size={20} color="black" />
+          </Pressable>
+        ) : (
+          <Pressable
+            style={styles.buttonContainer}
+            onPress={async () => {
+              if (!audioRecorder.uri) return;
+              await handleFileMessage("audio", "audio/mp4", [
+                audioRecorder.uri,
+              ]);
+              setIsVoice(false);
+            }}
+          >
+            <Ionicons name="send-outline" size={20} color={colors.WHITE} />
+          </Pressable>
+        )
+      }
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: colors.SAND_300,
+    padding: 8,
+    borderRadius: 5,
+  },
+  buttons: {
+    flexDirection: "row",
+    gap: 20,
+  },
+});
