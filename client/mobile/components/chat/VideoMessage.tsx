557143c23800fe5bdd6c6af3c3680d8bbf86053d
jungwook <mr4454@naver.com>
2026-04-30 18:59:51 +0900

    chore: upgrade to expo 55 for fix video message but not expo module dont work
    so video message is still not working.

---
+++ client/mobile/components/chat/VideoMessage.tsx
@@ -0,0 +1,36 @@
+import { Pressable, StyleSheet } from "react-native";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { useState } from "react";
+import { colors } from "@/constants";
+
+export default function VideoMessage({ url }: { url: string }) {
+  const player = useVideoPlayer(url, (player) => {
+    player.loop = true;
+  });
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  return (
+    <Pressable
+      onPress={() => {
+        setIsPlaying(!isPlaying);
+        if (isPlaying) {
+          player.pause();
+          return;
+        }
+        player.seekBy(0);
+        player.play();
+      }}
+    >
+      <VideoView style={styles.media} player={player} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  media: {
+    width: 220,
+    height: 220,
+    borderRadius: 8,
+    backgroundColor: colors.GRAY_100,
+  },
+});
