c6663270e58dcfed9a149c0b014647f90e56a731
jungwook <mr4454@naver.com>
2026-05-02 21:42:59 +0900

    fix: give delete action authority to lamda in terraform
    This is for clean-up process after resizing original image.

--- infra/terraform/lamda.tf
+++ infra/terraform/lamda.tf
@@ -32,7 +32,8 @@ resource "aws_iam_policy" "lambda_s3_policy" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
-          "s3:PutObject"
+          "s3:PutObject",
+          "s3:DeleteObject"
         ]
         Resource = "${aws_s3_bucket.chat_bucket.arn}/*"
       }
