5f1f6aede3c8d79b75bbc96a0e7152eea35d44f6
jungwook <mr4454@naver.com>
2026-06-12 14:37:15 +0900

    feat: set up for cloud
    Dockerfile is prepared.
    Valkey username password auth and tls set.
    I set previous commit maxmemory-policy as allkeys-lfu and didnt mention at commit message.
    This strategy is best for caching my map pin datas.

--- backend/offlineconversation/src/main/java/com/xcecv/offlineconversation/config/ValkeyGlideConfig.java
+++ backend/offlineconversation/src/main/java/com/xcecv/offlineconversation/config/ValkeyGlideConfig.java
@@ -2,9 +2,11 @@ package com.xcecv.offlineconversation.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.zaxxer.hikari.util.Credentials;
 import glide.api.GlideClient;
 import glide.api.models.configuration.GlideClientConfiguration;
 import glide.api.models.configuration.NodeAddress;
+import glide.api.models.configuration.ServerCredentials;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,18 +14,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ValkeyGlideConfig {
 
-  @Value("${valkey.host:localhost}")
+  @Value("${valkey.host}")
   private String host;
 
-  @Value("${valkey.port:6379}")
+  @Value("${valkey.port}")
   private int port;
+
+  @Value("${valkey.username}")
+  private String username;
+
+  @Value("${valkey.password}")
+  private String password;
+
 
   @Bean(destroyMethod = "close")
   public GlideClient glideClient() {
     GlideClientConfiguration config = GlideClientConfiguration.builder()
         .address(NodeAddress.builder().host(host).port(port).build())
-        // .useTLS(true) // Enable if using TLS
-        // .credentials(Credentials.builder().username("user").password("pass").build())
+        .useTLS(true)
+        .credentials(ServerCredentials.builder()
+            .username(username)
+            .password(password)
+            .build()
+        )
         .requestTimeout(2000)
         .build();
     return GlideClient.createClient(config).join();
