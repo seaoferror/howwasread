1d5f85198bd560a817b34aaf4505cb770a9990e2
jungwook <mr4454@naver.com>
2026-06-13 05:52:33 +0900

    fix: use sarama syncProducer instead of asyncProducer
    when I need to concurrent producing, I can use wg pattern with multiple goroutine.
    this is much nicer for error handling.

--- backend/common/producer/root.go
+++ backend/common/producer/root.go
@@ -12,11 +12,11 @@ import (
 )
 
 type Producer struct {
-	asyncProducer sarama.AsyncProducer
+	syncProducer sarama.SyncProducer
 }
 
 func NewProducer(clientIdPrefix string) *Producer {
-	asyncProducer, err := createProducer(clientIdPrefix)
+	syncProducer, err := createProducer(clientIdPrefix)
 	if err != nil {
 		slog.Error("fail to create producer",
 			"err", err,
@@ -24,12 +24,11 @@ func NewProducer(clientIdPrefix string) *Producer {
 		panic(err)
 	}
 	log.Print("success to create kafka producer")
-	kp := Producer{asyncProducer}
-
+	kp := Producer{syncProducer}
 	return &kp
 }
 
-func createProducer(clientIdPrefix string) (sarama.AsyncProducer, error) {
+func createProducer(clientIdPrefix string) (sarama.SyncProducer, error) {
 	cfg := sarama.NewConfig()
 	id, err := uuid.NewV7()
 	if err != nil {
@@ -55,15 +54,16 @@ func createProducer(clientIdPrefix string) (sarama.AsyncProducer, error) {
 
 	cfg.Producer.Return.Successes = true
 	cfg.Producer.Return.Errors = true
-	cfg.Producer.Compression = sarama.CompressionZSTD
+	cfg.Producer.Compression = sarama.CompressionSnappy
 	cfg.Producer.RequiredAcks = sarama.WaitForLocal
-	cfg.Producer.Retry.Max = 1_000_000_000
-	cfg.Producer.Retry.Backoff = time.Millisecond * 10
-	//cfg.Producer.Idempotent = true
-	//cfg.Producer.RequiredAcks = sarama.WaitForAll
-	//cfg.Net.MaxOpenRequests = 1
+	cfg.Producer.Idempotent = false
+	cfg.Producer.Flush.Messages = 100
+	cfg.Producer.Flush.Frequency = time.Millisecond * 5
+	cfg.Producer.Retry.Max = 3
+	cfg.Producer.Retry.Backoff = time.Millisecond * 250
+	cfg.Net.MaxOpenRequests = 5
 
-	return sarama.NewAsyncProducer([]string{os.Getenv("KAFKA_ADDRESS")}, cfg)
+	return sarama.NewSyncProducer([]string{os.Getenv("KAFKA_ADDRESS")}, cfg)
 }
 
 func (p *Producer) PushMessage(topic string, key, value []byte) error {
@@ -75,34 +75,29 @@ func (p *Producer) PushMessage(topic string, key, value []byte) error {
 		msg.Key = sarama.ByteEncoder(key)
 	}
 
-	p.asyncProducer.Input() <- &msg
-
-	select {
-	case succeedMsg := <-p.asyncProducer.Successes():
-		log.Print("Success to produce payload",
-			"partition", succeedMsg.Partition)
-		return nil
-	case err := <-p.asyncProducer.Errors():
-		log.Print("Failed to produce payload",
-			"err", err)
+	partition, _, err := p.syncProducer.SendMessage(&msg)
+	if err != nil {
+		log.Print("Failed to produce payload", "err", err)
 		return err
 	}
+	log.Print("Success to produce payload", "partition", partition)
+	return nil
 }
 
 func (p *Producer) Close() error {
-	return p.asyncProducer.Close()
+	return p.syncProducer.Close()
 }
 
-func (p *Producer) PushDeadLetter(reason error, originalTopic string, value []byte) error {
-	_ = []sarama.RecordHeader{
-		{Key: []byte("x-error-message"), Value: []byte(reason.Error())},
-		{Key: []byte("x-original-topic"), Value: []byte(originalTopic)},
-		{Key: []byte("x-failed-at"), Value: []byte(time.Now().Format(time.RFC3339))},
-	}
-	err := p.PushMessage("dlq", nil, value)
-	if err != nil {
-		slog.Error("fail to publish dead letter", "err", err)
-		return err
-	}
-	return nil
-}
+//func (p *Producer) PushDeadLetter(reason error, originalTopic string, value []byte) error {
+//	_ = []sarama.RecordHeader{
+//		{Key: []byte("x-error-message"), Value: []byte(reason.Error())},
+//		{Key: []byte("x-original-topic"), Value: []byte(originalTopic)},
+//		{Key: []byte("x-failed-at"), Value: []byte(time.Now().Format(time.RFC3339))},
+//	}
+//	err := p.PushMessage("dlq", nil, value)
+//	if err != nil {
+//		slog.Error("fail to publish dead letter", "err", err)
+//		return err
+//	}
+//	return nil
+//}
