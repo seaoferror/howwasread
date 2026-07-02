c51db2b04a7cebfef187f2d32a05e4a8de69fb19
jungwook <mr4454@naver.com>
2026-07-02 12:28:17 +0900

    fix: should pass pointer in ScanContext
    I pass value in there and now working in notification service's FindRoomNameById.

--- backend/notification/internal/repository/info.go
+++ backend/notification/internal/repository/info.go
@@ -19,7 +19,7 @@ func (r *Repository) FindNameById(ctx context.Context, id gocql.UUID) (name stri
 }
 
 func (r *Repository) FindRoomNameById(ctx context.Context, id gocql.UUID) (name string, err error) {
-	err = r.session.Query("SELECT name FROM chat_room_by_id", id).ScanContext(ctx, name)
+	err = r.session.Query("SELECT name FROM chat_room_by_id", id).ScanContext(ctx, &name)
 	if err != nil {
 		slog.Error("fail to select name from chat_room_by_id",
 			"err", err,
