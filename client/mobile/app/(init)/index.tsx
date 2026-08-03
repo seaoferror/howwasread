8828fc8c0482dc6cb91eac7e773a787bfba76c65
jungwook <mr4454@naver.com>
2026-08-04 00:46:25 +0900

    design: show splash screen like loading screen for init

--- client/mobile/app/(init)/index.tsx
+++ client/mobile/app/(init)/index.tsx
@@ -1,16 +1,31 @@
-import { Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import InitRoute from "@/components/auth/InitRoute";
+import { Image } from "expo-image";
+
+const AppIcon = require("@/assets/images/icon_transparent_background.png");
 
 export default function InitScreen() {
   return (
     <InitRoute>
-      <SafeAreaView>
-        <Text>loading</Text>
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.icon} source={AppIcon} />
       </SafeAreaView>
     </InitRoute>
   );
 }
-//
-// const styles = StyleSheet.create({
-// });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#082247",
+  },
+  icon: {
+    alignContent: "center",
+    justifyContent: "center",
+    width: 160,
+    height: 120,
+  },
+});
